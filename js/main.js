2adc23c8a4c3f62a507dae7f9ac31cad333cbc9c
Andrew Hsu <adhsu01@gmail.com>
2016-07-16 14:57:29 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var pipelineIcons = document.getElementsByClassName("pipeline-icon")
+// first = pipelineIcons[pipelineIcons.length-1]
+
+// firstBottom = first.getBoundingClientRect().bottom
+// line = document.getElementsByClassName("pipeline-line")[0]
+
+// line.style.top = firstBottom
